88fde91b29ea79bd6ed3234f7a9fd6112c236341
Yanis <yanis@Air-de-Yanis.lan>
2024-09-11 12:55:46 +0200

    Fitness programme website

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'synonym': ['Synonym', 'sans-serif'],
+        'amulya': ['Amulya', 'sans-serif'],
+      },
+  },
+  plugins: [],
+  }
+}
+
