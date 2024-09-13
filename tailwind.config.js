43b23d6d36f3f77efcfd9f33a86417b1891cb607
Yanismd27 <yanis.meddour27@gmail.com>
2024-09-13 15:07:56 +0200

    v-4

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,8 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js,jsx,ts,tsx}"
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        'synonym': ['Synonym', 'sans-serif'],
-        'amulya': ['Amulya', 'sans-serif'],
-      },
-  },
-  plugins: [],
+    content: ['./src/**/*.{html,js}', './src/input.css'],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
   }
-}
-
-
+  
